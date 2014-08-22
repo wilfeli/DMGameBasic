e6b5931c6b1888c632f4abbd39f55788f347e65b
wilfeli <wilfeli@gmail.com>
2014-08-22 16:14:40 -0500

    Create History.h

---
+++ src/History.h
@@ -0,0 +1,22 @@
+/*
+ * History.h
+ *
+ *  Created on: May 17, 2014
+ *      Author: wilfeli
+ */
+
+#ifndef HISTORY_H_
+#define HISTORY_H_
+
+
+
+class HistoryHF{
+	//collects history for the world
+
+
+
+};
+
+
+
+#endif /* HISTORY_H_ */
